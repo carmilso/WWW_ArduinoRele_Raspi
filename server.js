a092551a438cf427560bac61125ed95727d3b685
Carlos Millan <carmilso@inf.upv.es>
2014-11-25 16:00:00 +0100

    Afegit escriure en log les connexions al server

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ var ip = require('ip');
 var form = fs.readFileSync('form.html');
 
 http.createServer(function(request, response){
-  console.log("Client connectat -> " + request.connection.remoteAddress);
-  var a = new Date().toString();
-  console.log(a);
+  var ip = request.connection.remoteAddress;
+  console.log("Client connectat -> " + ip);
+  var data = new Date().toString();
+  console.log(data);
+
+  fs.appendFile('server.log', ip + " -> " + data + '\n', function(err){
+    if(err)
+      console.log("Error en escriure el log -> " + err);
+  });
+
   if(request.method == "GET"){
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(form);
