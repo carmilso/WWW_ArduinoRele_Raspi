67fadd8ce5878e4cfcd24444b3f825b049a9f4dc
Carlos Millan <carmilso@inf.upv.es>
2014-11-25 10:38:52 +0100

    Mostra l'hora local del server

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var ip = require('ip');
 var form = fs.readFileSync('form.html');
 
 http.createServer(function(request, response){
+  console.log("Client connectat -> " + request.connection.remoteAddress);
+  var a = new Date().toString();
+  console.log(a);
   if(request.method == "GET"){
-    console.log("Client connectat -> " + request.connection.remoteAddress);
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(form);
   }
